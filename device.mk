29f3012204ef342ce3bbcd77e976f2a52a7f5c4d
Mashopy <eliasgheeraert@gmail.com>
2025-02-04 15:15:31 +0100

    jade: configs: Import Spreadtrum/UniSoc IMS app permissions

--- device.mk
+++ device.mk
@@ -141,6 +141,9 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_COPY_FILES += \
+    device/jingpad/jade/configs/permissions/privapp-permissions-com.spreadtrum.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.spreadtrum.ims.xml
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.broadcastradio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.broadcastradio.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
