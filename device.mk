82a09b6903b086bcb255dd7fae2120fda351cb84
Mashopy <eliasgheeraert@gmail.com>
2025-02-05 12:16:51 +0100

    jade: Define screen dimensions for LineageOS boot animation

--- device.mk
+++ device.mk
@@ -36,6 +36,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2368
+TARGET_SCREEN_WIDTH := 1728
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
